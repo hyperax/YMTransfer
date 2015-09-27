ec77cc43335dd3a5487672db0d81a93efa85b560
Arthur <hyperax@gmail.com>
2015-09-27 20:25:01 +0300

    refactoring

--- app/src/main/java/ru/yandex/money/ymtransfer/properties/YMProperties.java
+++ app/src/main/java/ru/yandex/money/ymtransfer/properties/YMProperties.java
@@ -1,9 +1,21 @@
 package ru.yandex.money.ymtransfer.properties;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class YMProperties {
     public static final String CLIENT_ID = "B06CB2A09433850862BF5B13BFD474BAFC69DEDEA4244FC41998055B7C3264F5";
-    public static final BigDecimal FEE_PERCENT = new BigDecimal("0.5");
+    public static final BigDecimal FEE_VALUE = new BigDecimal("0.005");
 
+    public static final String HOST = "https://money.yandex.ru";
+
+    public static BigDecimal addFee(BigDecimal amount) {
+        return amount.multiply(FEE_VALUE.add(BigDecimal.ONE))
+                .setScale(2, RoundingMode.HALF_UP).stripTrailingZeros();
+    }
+
+    public static BigDecimal substractFee(BigDecimal amount) {
+        return amount.divide(FEE_VALUE.add(BigDecimal.ONE), RoundingMode.HALF_UP)
+                .setScale(2, RoundingMode.HALF_UP).stripTrailingZeros();
+    }
 }
--- app/src/main/java/ru/yandex/money/ymtransfer/view/fragments/TransferFragment.java
+++ app/src/main/java/ru/yandex/money/ymtransfer/view/fragments/TransferFragment.java
@@ -1,23 +1,34 @@
 package ru.yandex.money.ymtransfer.view.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.yandex.money.api.methods.params.P2pTransferParams;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.FocusChange;
 import org.androidannotations.annotations.FragmentArg;
 import org.androidannotations.annotations.InstanceState;
 import org.androidannotations.annotations.ViewById;
 
 import ru.softbalance.widgets.NumberEditText;
+import ru.yandex.money.android.PaymentActivity;
 import ru.yandex.money.ymtransfer.R;
 import ru.yandex.money.ymtransfer.data.model.Transfer;
 import ru.yandex.money.ymtransfer.data.sqlite.Storage;
+import ru.yandex.money.ymtransfer.properties.YMProperties;
 
 @EFragment(R.layout.fragment_transfer)
 public class TransferFragment extends BaseFragment {
+
+    private static final int REQUEST_PAYMENT = 1;
 
     @ViewById(R.id.recipient)
     EditText recipient;
@@ -28,11 +39,11 @@ public class TransferFragment extends BaseFragment {
     @ViewById(R.id.amount_due)
     NumberEditText amountDue;
 
-    @ViewById(R.id.comment)
-    EditText comment;
-
     @ViewById(R.id.use_code_protection)
     CheckBox useCodeProtection;
+
+    @ViewById(R.id.comment)
+    EditText comment;
 
     @ViewById(R.id.receive_period)
     NumberEditText receivePeriod;
@@ -72,8 +83,8 @@ public class TransferFragment extends BaseFragment {
         transfer.setRecipient(recipient.getText().toString().trim());
         transfer.setAmountPayment(amountPay.getValue());
         transfer.setAmountDue(amountDue.getValue());
-        transfer.setComment(comment.getText().toString().trim());
         transfer.setIsCodeProtection(useCodeProtection.isChecked());
+        transfer.setComment(comment.getText().toString().trim());
         transfer.setReceivePeriod(receivePeriod.getValue().intValue());
     }
 
@@ -84,11 +95,48 @@ public class TransferFragment extends BaseFragment {
         startPayment();
     }
 
+    @FocusChange(R.id.amount_pay)
+    void onAmountPayChanged(boolean isFocused, View view) {
+        if (!isFocused) {
+            amountDue.setValue(YMProperties.substractFee(amountPay.getValue()));
+        }
+    }
+
+    @FocusChange(R.id.amount_due)
+    void onAmountDueChanged(boolean isFocused, View view) {
+        if (!isFocused) {
+            amountPay.setValue(YMProperties.addFee(amountDue.getValue()));
+        }
+    }
+
     private void savePayment() {
         Storage.get().put(transfer);
     }
 
     private void startPayment() {
+        P2pTransferParams payParameters = new P2pTransferParams.Builder(transfer.getRecipient())
+                .setAmount(transfer.getAmountPayment())
+                .setCodepro(transfer.isCodeProtection())
+                .setMessage(transfer.getComment())
+                .setExpirePeriod(transfer.getReceivePeriod())
+                .build();
+        Intent intent = PaymentActivity.getBuilder(getActivity())
+                .setPaymentParams(payParameters)
+                .setClientId(YMProperties.CLIENT_ID)
+                .setHost(YMProperties.HOST)
+                .build();
+        startActivityForResult(intent, REQUEST_PAYMENT);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_PAYMENT && resultCode == Activity.RESULT_OK) {
+            Toast.makeText(getActivity(), R.string.transfer_completed, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getActivity(), R.string.transfer_incompleted, Toast.LENGTH_LONG).show();
+        }
 
     }
 }
