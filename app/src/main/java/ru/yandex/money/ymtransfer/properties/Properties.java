5bf03ef544108e4b0073d2b92d398311b0ade548
Arthur <hyperax@gmail.com>
2015-09-27 11:42:43 +0300

    init

---
+++ app/src/main/java/ru/yandex/money/ymtransfer/properties/Properties.java
@@ -0,0 +1,6 @@
+package ru.yandex.money.ymtransfer.properties;
+
+public class Properties {
+    public static final String CLIENT_ID = "B06CB2A09433850862BF5B13BFD474BAFC69DEDEA4244FC41998055B7C3264F5";
+
+}
---
+++ app/src/main/java/ru/yandex/money/ymtransfer/view/TransferFragment.java
@@ -0,0 +1,14 @@
+package ru.yandex.money.ymtransfer.view;
+
+import android.support.v4.app.Fragment;
+
+import org.androidannotations.annotations.EFragment;
+
+import ru.yandex.money.ymtransfer.R;
+
+@EFragment(R.layout.fragment_transfer)
+public class TransferFragment extends Fragment {
+
+
+
+}
